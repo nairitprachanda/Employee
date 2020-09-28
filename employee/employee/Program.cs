cf1120d70bd5ca1ae25e6f5ad01c0b631066aa35
nairit <nairitprachanda@gmail.com>
2020-09-28 22:13:05 +0530

    [Nairit] Refactor:Wage

--- employee/employee/Program.cs
+++ employee/employee/Program.cs
@@ -6,16 +6,27 @@ namespace employee
     {
         static void Main(string[] args)
         {
-              
-                int IS_FULL_TIME = 1;
-                Random random = new Random();
-                int empCheck = random.Next(0, 2);
-                if (empCheck == IS_FULL_TIME)
-                    Console.WriteLine("Employee is Present");
-                else
-                    Console.WriteLine("Employee is absent");
-                Console.ReadKey();
+            int IsFullTime = 1;
+            int EmpRatePerHour = 20;
+            //Variables
+            int empWage = 0;
+            int empHr = 0;
+            //Calculation
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IsFullTime)
+            {
+                Console.WriteLine("Employee is Present.");
+                empHr = 8;
             }
+            else
+            {
+                Console.WriteLine("Employee is Absent!");
+                empHr = 0;
+            }
+            empWage = empHr * EmpRatePerHour;
+            Console.WriteLine("Employee Wage = " + empWage);
+        }
         
     }
 }
