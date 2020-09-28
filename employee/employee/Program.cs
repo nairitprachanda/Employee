3c7bc3e4045bf654f6c28bf89b210a342ed6fd82
nairit <nairitprachanda@gmail.com>
2020-09-28 22:37:29 +0530

    [] Refactor:Time condition

--- employee/employee/Program.cs
+++ employee/employee/Program.cs
@@ -3,12 +3,13 @@
 namespace employee
 {
     class Program
-    {{
+    {
         //Constants
         public const int IsPartTime = 0;
         public const int IsFullTime = 1;
         public const int EmpRatePerHour = 20;
         public const int NumOfWorkingDays = 20;
+        public const int TotalNumOfHours = 100;
         static void Main(string[] args)
         {
             //Welcome msg
@@ -17,8 +18,10 @@ namespace employee
             int empWage = 0;
             int empHr = 0;
             int empWageTotal = 0;
+            int workingHrs = 0;
+            int day = 0;
 
-            for (int day = 0; day < NumOfWorkingDays; day++)
+            for (day = 0; day < NumOfWorkingDays && workingHrs < TotalNumOfHours; day++)
             {
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
@@ -27,18 +30,22 @@ namespace employee
                 {
                     case IsFullTime:
                         empHr = 8;
+                        workingHrs += empHr;
                         break;
                     case IsPartTime:
                         empHr = 4;
+                        workingHrs += empHr;
                         break;
                     default:
                         empHr = 0;
+                        workingHrs += empHr;
                         break;
                 }
                 empWage = empHr * EmpRatePerHour;
                 empWageTotal += empWage;
                 Console.WriteLine("Employee Wage = " + empWage + " on day " + (day + 1));
             }
+            Console.WriteLine("Total days worked = " + day + " Total hours worked = " + workingHrs);
             Console.WriteLine("Monthly Employee Wage = " + empWageTotal);
 
         }
